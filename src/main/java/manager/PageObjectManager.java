0e5ecfe59cef4d9f0d941d5e029af9311d3093a3
Saroj <sarojkumar@handletechlabs.com>
2026-04-16 15:10:10 +0530

    all stock

--- src/main/java/manager/PageObjectManager.java
+++ src/main/java/manager/PageObjectManager.java
@@ -13,21 +13,9 @@ public class PageObjectManager {
 
     private WebDriver driver;
     private Hooks hooks;
-    private NegativeSignupPages negsignups;
-    private MyOrdersPage myorders;
     private LoginPage login;
     private HomePage home;
-    private Menus menu;
-    private ProductListingPage pLp;
-    private CheckoutPage cOp;
-    private SaleOffer50Percentage sale;
-    private SaleBuy1Get1 saleB1G1;
-    private OrdersPage order;
     private AddressPage address;
-    private FooterPage footer;
-    private ProductDetailsPage pDP;
-    private SearchSectionPage search;
-    private CouponPage coupon;
     private AdminPanelPage admin;
     private AdminPanelSortingPage adminSort;
     private AdminPanelExportExcelFileMatchPage adminExport;
@@ -43,6 +31,7 @@ public class PageObjectManager {
     private TryAlongSectionPage tryAlong;
     private Reg_TrackOrder_Page trackOrder;
     private Landing_Page land;
+    private StockQuantityPage stock;
 
    
 
@@ -53,29 +42,11 @@ public class PageObjectManager {
         return (address == null) ? address = new AddressPage(driver) : address;
     }
     
-    public FooterPage getFooterPage() {
-        return (footer == null) ? footer = new FooterPage(driver) : footer;
-    }
-    
-    public ProductDetailsPage getProductDetailsPage() {
-        return (pDP == null) ? pDP = new ProductDetailsPage(driver) : pDP;
-    }
-    public SearchSectionPage getSearchSectionPage() {
-        return (search == null) ? search = new SearchSectionPage(driver) : search;
-    }
-    
-    public NegativeSignupPages getNegativeSignupPages() {
-        return (negsignups == null) ? negsignups = new NegativeSignupPages(driver) : negsignups;
-    }
 	public Hooks getHooks() {
         TestContext testContext = null;
 		return (hooks == null) ? hooks = new Hooks(testContext) : hooks;
     }
-    
-	public MyOrdersPage getMyOrdersPage() {
-        return (myorders == null) ? myorders = new MyOrdersPage(driver) : myorders;
-    
-	}
+  
     
 	public LoginPage getLoginPage() {
         return (login == null) ? login = new LoginPage(driver) : login;
@@ -86,39 +57,7 @@ public class PageObjectManager {
         return (home == null) ? home = new HomePage(driver) : home;
     
 	}
-    
-	public Menus getMenus() {
-        return (menu == null) ? menu = new Menus(driver) : menu;
-    
-	}
-    
-	public ProductListingPage getProductListingPage() {
-        return (pLp == null) ? pLp = new ProductListingPage(driver) : pLp;
-    
-	}
-	
-	public CheckoutPage getCheckoutPage() {
-        return (cOp == null) ? cOp = new CheckoutPage(driver) : cOp;
-    
-	}
-    
-	public SaleOffer50Percentage getSaleOffer50Percentage() {
-        return (sale == null) ? sale = new SaleOffer50Percentage(driver) : sale;
-    
-	}
-	public SaleBuy1Get1 getSaleBuy1Get1() {
-        return (saleB1G1 == null) ? saleB1G1 = new SaleBuy1Get1(driver) : saleB1G1;
-    
-	}
-	public OrdersPage getOrdersPage() {
-        return (order == null) ? order = new OrdersPage(driver) : order;
-    
-	}
-	public CouponPage getCouponPage() {
-        return (coupon == null) ? coupon = new CouponPage(driver) : coupon;
-    
-	}
-	
+
 	public AdminPanelPage getAdminPanelPage() {
         return (admin == null) ? admin = new AdminPanelPage(driver) : admin;
     
@@ -177,5 +116,8 @@ public class PageObjectManager {
 	
 	public Landing_Page getLanding_Page() {
 		return (land == null) ? land = new Landing_Page(driver): land;
+	}
+	public StockQuantityPage getStockQuantityPage() {
+		return (stock == null) ? stock = new StockQuantityPage(driver): stock;
 	}
 }
---
+++ src/main/java/stepDef/StockQuantityStepDef.java
@@ -0,0 +1,40 @@
+package stepDef;
+
+import context.TestContext;
+import io.cucumber.java.en.*;
+import pages.StockQuantityPage;
+
+public class StockQuantityStepDef {
+	TestContext testContext;
+	StockQuantityPage stock;
+	
+	public StockQuantityStepDef(TestContext context) {
+		testContext = context;
+		stock = testContext.getPageObjectManager().getStockQuantityPage();
+	}
+
+		@Given("the stock quantity for the product is greater than {int}")
+		public void the_stock_quantity_for_the_product_is_greater_than(Integer int1) throws InterruptedException {
+			stock.validateStockQuantity();
+		}
+
+		@Then("the order is placed successfully")
+		public void the_order_is_placed_successfully() {
+		   
+		}
+
+
+			@Given("the product stock is equal to {int}")
+			public void the_product_stock_is_equal_to(Integer int1) throws InterruptedException {
+			  
+			}
+			@Then("the order is not placed and shows Out of Stock")
+			public void the_order_is_not_placed_and_shows_out_of_stock() {
+		
+			}
+
+
+
+	
+
+}
